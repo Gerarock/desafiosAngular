1328970a59da2ebb4cafd4aaa15ecbf7afbea853
gveliz90 <gveliz90@bancoestado.cl>
2023-05-04 08:58:44 -0400

    commit con unsubscribe

--- src/app/components/dashboard/pages/llamada-api-rest/llamada-api-rest.component.ts
+++ src/app/components/dashboard/pages/llamada-api-rest/llamada-api-rest.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { LlamadaApiRestService } from 'src/app/services/llamada-api-rest.service';
 import { MatTableDataSource } from '@angular/material/table';
 import { Clientes } from 'src/app/models/clientes.model';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-llamada-api-rest',
   templateUrl: './llamada-api-rest.component.html',
   styleUrls: ['./llamada-api-rest.component.css']
 })
-export class LlamadaApiRestComponent implements OnInit {
+export class LlamadaApiRestComponent implements OnInit, OnDestroy {
 
+  destroyed$ = new Subject<void>();
   dataSource = new MatTableDataSource<Clientes>();
   displayedColumns: string[] = ['id', 'nombre', 'apellido', 'edad', 'direccion', 'email'];
 
@@ -29,6 +31,11 @@ export class LlamadaApiRestComponent implements OnInit {
       }
     })
     this.llamadaApiRestService.obtenerClientes();
+  }
+
+  ngOnDestroy(): void {
+    this.destroyed$.next();
+    this.destroyed$.complete();
   }
 
 }
